017a075b5eaa4ed39adb8eee4b393bfa342e0306
Evgen Polivanov <polivanov004@gmail.com>
2025-10-04 20:43:00 +0700

    write signals and fix test for profile

--- sf_auth/apps.py
+++ sf_auth/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class SfAuthConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'sf_auth'
+
+    def ready(self):
+        import sf_auth.signals
