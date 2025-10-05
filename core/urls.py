81ba4f44ffd5ec26f45d0cfcc1ab19abda7f7a74
Evgen Polivanov <polivanov004@gmail.com>
2025-10-05 15:20:50 +0700

    add telegram authentication

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('sf_auth.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    re_path('^social/', include('social_django.urls', namespace='social')),
+    re_path(r'^auth/', include('drf_social_oauth2.urls', namespace='drf')),
 ]
